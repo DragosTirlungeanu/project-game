d65a7ba39af074ea7a292b9bdb757f1d66650808
Dragos Tirlungeanu <Dragos@Dragoss-MacBook-Pro.local>
2017-12-18 10:46:39 +0000

    start-up

---
+++ game.js
@@ -0,0 +1,51 @@
+function load() {
+  var puzzle = '<div id="slidingpuzzleContainer4x4">';
+
+  for (var i = 0; i <= 15; ++i) {
+    puzzle += '<img src="x.png" alt="smiley.png" width="98" height="98" />';
+  }
+  puzzle += '</div>';
+  showSlidingpuzzle(puzzle);
+}
+
+
+function changeFormat(x, y) {
+  var puzzlepieces = [];
+  var finalValue = x * y - 2;
+
+  for (var i = 0; i <= finalValue; ++i) {
+    puzzlepieces.push(i);
+  }
+
+  puzzlepieces.push('blank')
+  createSlidingpuzzle(puzzlepieces, x, y);
+}
+
+
+function createSlidingpuzzle(puzzlepieces, x, y) {
+
+  var puzzle = '<div id="slidingpuzzleContainer' + x + 'x' + y + '">';
+
+  for (var puzzleNr = 0; puzzleNr < puzzlepieces.length; ++puzzleNr) {
+    puzzle += '<img src="images/' + puzzlepieces[puzzleNr] + '.jpg" class="puzzlepiece" id="position' + puzzlepieces[puzzleNr] + '" alt="' + puzzlepieces[puzzleNr] + '" onclick="shiftPuzzlepieces(this);" width="100" height="100" />';
+  }
+  puzzle += '</div>';
+
+  showSlidingpuzzle(puzzle);
+}
+
+
+function showSlidingpuzzle(puzzle) {
+  document.getElementById('slidingpuzzleContainer').innerHTML = puzzle;
+}
+
+
+function shiftPuzzlepieces(el) {
+
+var blank = document.getElementById("positionblank"); 
+var temp = el.parentNode.insertBefore(document.createElement('a'), el); 
+el.parentNode.insertBefore(el, blank); 
+el.parentNode.insertBefore(blank, temp); 
+el.parentNode.removeChild(temp); 
+
+}
