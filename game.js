659051176eeee18649a195644ca9c7e1e505b57c
Tech-A46 <tech-a46@Tech-A46s-MacBook-Air.local>
2017-12-19 11:02:54 +0000

    added shuffle button

--- game.js
+++ game.js
@@ -1,16 +1,26 @@
 $(load);
 
-$('#shuffleBoard').click(shuffleBoard);
+//   function(){
+//   var puzzle = $('<div id="formatContainer">');
+//   for (var i = 0; i <= 15; ++i) {
+//   puzzle += '<img src="gid.gif" alt="blank" width="100" height="98" />';
+//   }
+//   puzzle += '</div>';
+//   var pieces = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+//   createSlidingpuzzle(shuffle(pieces), 4, 4);
+// }
 
 function load() {
-  var puzzle = $('<div id="formatContainer">');
-  for (var i = 0; i <= 15; ++i) {
+  $('#shuffleBoard').click(function() {
+    // var puzzle = $('<div id="formatContainer">');
+    // for (var i = 0; i <= 15; ++i) {
     // puzzle += '<img src="gid.gif" alt="blank" width="100" height="98" />';
-  }
-  puzzle += '</div>';
-  // showSlidingpuzzle(puzzle);
-  var pieces = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
-  createSlidingpuzzle(shuffle(pieces), 4, 4);
+    // }
+    // puzzle += '</div>';
+    // showSlidingpuzzle(puzzle);
+    var pieces = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,"blank"];
+    createSlidingpuzzle(shuffle(pieces), 4, 4);
+  });
 
 }
 
@@ -18,49 +28,42 @@ function load() {
 
 function shuffle(array) {
   var copy = [], n = array.length, i;
-
-  // While there remain elements to shuffle…
   while (n) {
-
-    // Pick a remaining element…
     i = Math.floor(Math.random() * n--);
-
-    // And move it to the new array.
     copy.push(array.splice(i, 1)[0]);
   }
-
   return copy;
 }
 
-function shuffleBoard() {
-  for (var i = 0; i < puzzlepieces; ++i) {
-    for (var j = 0; j < puzzlepieces; ++j) {
-      var k = Math.floor(Math.random() * puzzlepieces);
-      var l = Math.floor(Math.random() * puzzlepieces);
-      swapTiles(i, j, k, l);
-    }
-  }
-}
+// function shuffleBoard() {
+//   for (var i = 0; i < puzzlepieces; ++i) {
+//     for (var j = 0; j < puzzlepieces; ++j) {
+//       var k = Math.floor(Math.random() * puzzlepieces);
+//       var l = Math.floor(Math.random() * puzzlepieces);
+//       swapTiles(i, j, k, l);
+//     }
+//   }
+// }
 
-function swapTiles(i, j, k, l) {
-  var temp = new Object();
-  temp = puzzlepieces[i][j];
-  puzzlepieces[i][j] = puzzlepieces[k][l];
-  puzzlepieces[k][l] = temp;
-  location.reload();
-}
+// function swapTiles(i, j, k, l) {
+//   var temp = new Object();
+//   temp = puzzlepieces[i][j];
+//   puzzlepieces[i][j] = puzzlepieces[k][l];
+//   puzzlepieces[k][l] = temp;
+//   location.reload();
+// }
 
-// <------ ENDS THE SHUFFLE
+// // <------ ENDS THE SHUFFLE
 
-function changeFormat(x, y) {
-  var puzzlepieces = [];
-  var finalValue = x * y - 2;
-  for (var i = 0; i <= finalValue; ++i) {
-    puzzlepieces.push(i);
-  }
-  puzzlepieces.push('blank')
-  createSlidingpuzzle(puzzlepieces, x, y);
-}
+// function changeFormat(x, y) {
+//   var puzzlepieces = [];
+//   var finalValue = x * y - 2;
+//   for (var i = 0; i <= finalValue; ++i) {
+//     puzzlepieces.push(i);
+//   }
+//   puzzlepieces.push('blank')
+//   createSlidingpuzzle(puzzlepieces, x, y);
+// }
 
 
 function createSlidingpuzzle(puzzlepieces, x, y) {
@@ -89,7 +92,7 @@ function showSlidingpuzzle(puzzle) {
 
 function shiftPuzzlepieces(el) {
 
-  var blank = document.getElementById("positionblank"); 
+  var blank = $("positionblank"); 
   var temp = el.parentNode.insertBefore(document.createElement('a'), el); 
   el.parentNode.insertBefore(el, blank); 
   el.parentNode.insertBefore(blank, temp); 
